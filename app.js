531e436f6c79520d4cd1eb6cf5eda10798a38ffc
Abdisamatov Ozodbek <abdisamatovozodbek003@gmail.com>
2022-11-18 15:39:41 +0500

    salom

---
+++ app.js
@@ -0,0 +1,24 @@
+//Data
+//Primitive
+//String , Number, Bollean, underfined, null
+//alert ("salom");
+//console.log(NaN);
+//Variables
+//var , let, const
+//var score = 30;
+//console.log(score);
+//score = [];
+//console.log(score);
+//score = 50;
+alert("O`zingiz haqida malumotlarni kiriting!");
+var username = prompt('Ismingizni kiriting!');
+console.log("Ismi:" + username);
+var userlast = prompt("Familiyangizni kiriting!");
+console.log("Familiyasi:" + userlast);
+var userage = prompt("Yoshingiz kirting!");
+console.log("Yoshi:" + userage);
+var userfrom = prompt("Doimiy yashash joyingiz!");
+console.log("Doimiy yashash joyi:" + userfrom);
+var useremail = prompt("Eamilingizni kiriting!");
+console.log("Emaili:" + useremail);
+alert("Hammasi uchun sizga katta rahmat!😊 ");
